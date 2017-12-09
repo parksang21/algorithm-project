06b9cf149586df5f4f1cf8b870bc094daec1160e
Steve Prak <parksang21@skku.edu>
2017-12-09 14:05:45 +0900

    DB  설계 끝

--- root/classes/admin.py
+++ root/classes/admin.py
@@ -3,5 +3,12 @@ from .models import ClassModel,RegisterModel
 # Register your models here.
 
 
-admin.site.register(ClassModel)
-admin.site.register(RegisterModel)
+@admin.register(ClassModel)
+class ClassAdmin(admin.ModelAdmin):
+    # list_display = ['title', 'class_code', 'time', 'prof']
+    pass
+
+@admin.register(RegisterModel)
+class RegisterAdmin(admin.ModelAdmin):
+    # list_distplay = ['class_model', 'student', 'register_date']
+    pass
--- root/student/models.py
+++ root/student/models.py
@@ -7,6 +7,7 @@ from django.conf import settings
 class StudentModel(models.Model):
     name = models.CharField(max_length=20)
     student_id = models.IntegerField(unique=True)
+    year = models.IntegerField(default=1)
 
     def __str__(self):
         return self.name + '-' + str(self.student_id)
